83dac1b4b52a40c20e7ed228d1666086cff78e45
Tagir Enikeev <enikeev.tg@gmail.com>
2023-06-10 16:57:02 +0300

    head comment in memchr

--- src/e_string_src/1_e_memchr.c
+++ src/e_string_src/1_e_memchr.c
@@ -1,16 +1,19 @@
-/*
- *  src/e_string/e_memchr.c
- *  (c) T. Enikeev
- *  enikeev.tg@gmail.com
+/**
+ * src/e_string/e_memchr.c
+ * (c) T. Enikeev
+ * enikeev.tg@gmail.com
+ * 
+ * @brief Searching for the first occurrence of the character c
+ * (an unsigned char) in the first n bytes of the string pointed to,
+ * by the argument str.
+ * @warning Argument n must be positive and less or equal than length str.
+ * @return Pointer to the first occurrence of the character c
+ * in the first n bytes of the str or E_NULL pointer if str doesn't contain
+ * character c.
  */
 
 #include "../e_string.h"
 
-/*==============================================================================
-              1 void* memchr(const void* str, int c, size_t n):
-    Searching for the first occurrence of the character c (an unsigned char)
-      in the first n bytes of the string pointed to, by the argument str.
-==============================================================================*/
 void* e_memchr(const void* str, int c, e_size_t n) {
   char* ptr = (char*)str;
   for (e_size_t i = 0; i < n && *ptr != c && *ptr; i++) ptr++;
